3d9ff9ba2a4f6dbcbfa23ee2e930bae1b7ca5cb9
MN-Hoai <hoaimn.cntt@gmail.com>
2025-11-15 15:03:15 +0700

    ok

--- OIDC_Management/AuthorizationServer/Program.cs
+++ OIDC_Management/AuthorizationServer/Program.cs
@@ -39,5 +39,11 @@ app.UseStaticFiles();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 app.Run();
